608b5ff7e8297b8802bdbc1aa97a411e862dd3e1
teaearlgraycold <teaearlgraycold@protonmail.com>
2020-07-15 12:53:28 -0700

    WIP - add PromisePool concept

--- src/runnable/jit_target.rs
+++ src/runnable/jit_target.rs
@@ -1,5 +1,10 @@
 use std::collections::VecDeque;
-use std::mem;
+use std::{
+    cell::RefCell,
+    mem,
+    ops::{Index, IndexMut},
+    rc::Rc,
+};
 
 use super::super::code_gen;
 use super::super::parser::ASTNode;
@@ -69,16 +74,86 @@ pub type JITPromiseID = usize;
 
 /// Holds ASTNodes for later compilation.
 #[derive(Debug)]
-pub enum JITPromise {
+enum JITPromise {
     Deferred(VecDeque<ASTNode>),
     Compiled(JITTarget),
+}
+
+impl JITPromise {
+    pub fn source(&self) -> &VecDeque<ASTNode> {
+        match self {
+            JITPromise::Deferred(source) => source,
+            JITPromise::Compiled(JITTarget { source, .. }) => source,
+        }
+    }
+}
+
+impl PartialEq for JITPromise {
+    fn eq(&self, other: &Self) -> bool {
+        self.source() == other.source()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct PromisePool {
+    promises: Rc<RefCell<Vec<JITPromise>>>,
+}
+
+impl PromisePool {
+    pub fn new() -> Self {
+        PromisePool {
+            promises: Rc::new(RefCell::new(Vec::new())),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.promises.borrow().len()
+    }
+
+    pub fn find(&self, new_promise: &JITPromise) -> Option<(usize, &JITPromise)> {
+        self.promises
+            .borrow()
+            .iter()
+            .enumerate()
+            .find(|&(_index, promise)| promise == new_promise)
+    }
+
+    pub fn add(&mut self, new_promise: &JITPromise) -> JITPromiseID {
+        let vec = self.promises.borrow_mut();
+
+        if let Some((index, _)) = vec
+            .iter()
+            .enumerate()
+            .find(|&(_index, promise)| promise == new_promise)
+        {
+            return index;
+        }
+
+        vec.push(*new_promise);
+        return vec.len() - 1;
+    }
+}
+
+impl Index<usize> for PromisePool {
+    type Output = JITPromise;
+
+    fn index(&self, id: JITPromiseID) -> &Self::Output {
+        &self.promises.borrow()[id]
+    }
+}
+
+impl IndexMut<usize> for PromisePool {
+    fn index_mut(&mut self, id: JITPromiseID) -> &mut Self::Output {
+        &mut self.promises.borrow_mut()[id]
+    }
 }
 
 /// Container for executable bytes.
 #[derive(Debug)]
 pub struct JITTarget {
+    source: VecDeque<ASTNode>,
     bytes: Vec<u8>,
-    promises: Vec<JITPromise>,
+    promise_pool: PromisePool,
 }
 
 impl JITTarget {
@@ -86,13 +161,17 @@ impl JITTarget {
     #[cfg(target_arch = "x86_64")]
     pub fn new(nodes: &VecDeque<ASTNode>) -> Result<Self, String> {
         let mut bytes = Vec::new();
-        let mut promises = Vec::new();
+        let promise_pool = PromisePool::new();
 
-        code_gen::wrapper(&mut bytes, Self::shallow_compile(nodes, &mut promises));
+        code_gen::wrapper(
+            &mut bytes,
+            Self::shallow_compile(nodes, &mut promise_pool.clone()),
+        );
 
         Ok(Self {
+            source: nodes.clone(),
             bytes: make_executable(&bytes),
-            promises,
+            promise_pool,
         })
     }
 
@@ -103,15 +182,18 @@ impl JITTarget {
     }
 
     #[cfg(target_arch = "x86_64")]
-    fn new_fragment(nodes: &VecDeque<ASTNode>) -> Self {
+    fn new_fragment(nodes: &VecDeque<ASTNode>, promise_pool: PromisePool) -> Self {
         let mut bytes = Vec::new();
-        let mut promises = Vec::new();
 
-        code_gen::wrapper(&mut bytes, Self::compile_loop(nodes, &mut promises));
+        code_gen::wrapper(
+            &mut bytes,
+            Self::compile_loop(nodes, &mut promise_pool.clone()),
+        );
 
         Self {
+            source: nodes.clone(),
             bytes: make_executable(&bytes),
-            promises,
+            promise_pool,
         }
     }
 
@@ -119,7 +201,7 @@ impl JITTarget {
     ///
     /// r10 is used to hold the data pointer.
     #[cfg(target_arch = "x86_64")]
-    fn shallow_compile(nodes: &VecDeque<ASTNode>, promises: &mut Vec<JITPromise>) -> Vec<u8> {
+    fn shallow_compile(nodes: &VecDeque<ASTNode>, promise_pool: &mut PromisePool) -> Vec<u8> {
         let mut bytes = Vec::new();
 
         for node in nodes {
@@ -131,9 +213,9 @@ impl JITTarget {
                 ASTNode::Print => code_gen::print(&mut bytes, jit_functions::print),
                 ASTNode::Read => code_gen::read(&mut bytes, jit_functions::read),
                 ASTNode::Loop(nodes) if nodes.len() >= INLINE_THRESHOLD => {
-                    bytes.extend(Self::defer_loop(nodes, promises))
+                    bytes.extend(Self::defer_loop(nodes, promise_pool))
                 }
-                ASTNode::Loop(nodes) => bytes.extend(Self::compile_loop(nodes, promises)),
+                ASTNode::Loop(nodes) => bytes.extend(Self::compile_loop(nodes, promise_pool)),
             };
         }
 
@@ -142,22 +224,36 @@ impl JITTarget {
 
     /// Perform AOT compilation on a loop.
     #[cfg(target_arch = "x86_64")]
-    fn compile_loop(nodes: &VecDeque<ASTNode>, promises: &mut Vec<JITPromise>) -> Vec<u8> {
+    fn compile_loop(nodes: &VecDeque<ASTNode>, promise_pool: &mut PromisePool) -> Vec<u8> {
         let mut bytes = Vec::new();
 
-        code_gen::aot_loop(&mut bytes, Self::shallow_compile(nodes, promises));
+        code_gen::aot_loop(&mut bytes, Self::shallow_compile(nodes, promise_pool));
 
         bytes
     }
 
     /// Perform JIT compilation on a loop.
     #[cfg(target_arch = "x86_64")]
-    fn defer_loop(nodes: &VecDeque<ASTNode>, promises: &mut Vec<JITPromise>) -> Vec<u8> {
+    fn defer_loop(nodes: &VecDeque<ASTNode>, promise_pool: &mut PromisePool) -> Vec<u8> {
         let mut bytes = Vec::new();
+        let new_promise = JITPromise::Deferred(nodes.clone());
 
-        promises.push(JITPromise::Deferred(nodes.clone()));
+        // If an identical promise is already in the pool, use that existing
+        // promise instead of adding the new one.
+        if let Some((id, promise)) = promise_pool.find(&new_promise) {
+            match promise {
+                JITPromise::Compiled(jit_target) => {
+                    code_gen::aot_loop(&mut bytes, jit_target.bytes.clone());
+                }
+                JITPromise::Deferred(_source) => {
+                    code_gen::jit_loop(&mut bytes, id);
+                }
+            };
 
-        code_gen::jit_loop(&mut bytes, promises.len() - 1);
+            return bytes;
+        }
+
+        code_gen::jit_loop(&mut bytes, promise_pool.add(&new_promise));
 
         bytes
     }
@@ -178,19 +274,21 @@ impl JITTarget {
     /// targets to be compiled, ran, and later re-ran.
     #[cfg(target_arch = "x86_64")]
     extern "C" fn jit_callback(&mut self, loop_index: JITPromiseID, mem_ptr: *mut u8) -> *mut u8 {
-        let promise = &mut self.promises[loop_index];
-        let return_ptr;
+        let mut return_ptr = mem_ptr;
+        let mut new_target = None;
 
-        match promise {
-            JITPromise::Deferred(nodes) => {
-                let mut new_target = Self::new_fragment(nodes);
-                return_ptr = new_target.exec(mem_ptr);
-                *promise = JITPromise::Compiled(new_target);
-            }
-            JITPromise::Compiled(jit_target) => {
-                return_ptr = jit_target.exec(mem_ptr);
-            }
-        };
+        if let JITPromise::Deferred(nodes) = &self.promise_pool[loop_index] {
+            new_target = Some(Self::new_fragment(&nodes, self.promise_pool.clone()));
+            return_ptr = new_target.unwrap().exec(mem_ptr);
+        }
+
+        if let JITPromise::Compiled(jit_target) = &mut self.promise_pool[loop_index] {
+            return_ptr = jit_target.exec(mem_ptr);
+        }
+
+        // if let Some(jit_target) = &new_target {
+        //     self.promise_pool[loop_index] = JITPromise::Compiled(jit_target);
+        // }
 
         return_ptr
     }
