b33a66a5c0b0e1e04187d82ed19801517280c55b
Daniel Angell <me@danangell.com>
2025-01-31 16:36:04 -0800

    Use div_ceil method

--- src/runnable/jit/jit_helpers.rs
+++ src/runnable/jit/jit_helpers.rs
@@ -8,21 +8,13 @@ use libc::{sysconf, _SC_PAGESIZE};
 
 static PAGE_SIZE: OnceLock<usize> = OnceLock::new();
 
-/// Round up an integer division.
-///
-/// * `numerator` - The upper component of a division
-/// * `denominator` - The lower component of a division
-fn int_div_ceil(numerator: usize, denominator: usize) -> usize {
-    (numerator / denominator + 1) * denominator
-}
-
 /// Allocate a buffer of executable memory pages.
 fn allocate_buffer(length: usize) -> Vec<u8> {
     let mut buffer = mem::MaybeUninit::<*mut libc::c_void>::uninit();
     let buffer_ptr = buffer.as_mut_ptr();
 
     let page_size = *PAGE_SIZE.get_or_init(|| unsafe { sysconf(_SC_PAGESIZE) as usize });
-    let buffer_size = int_div_ceil(length, page_size);
+    let buffer_size = length.div_ceil(page_size);
 
     unsafe {
         libc::posix_memalign(buffer_ptr, page_size, buffer_size);
