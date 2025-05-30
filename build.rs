8515bacf6e5840d9b2ab9e2a32392382457da69e
Daniel Angell <me@danangell.com>
2025-05-30 10:57:12 +0100

    Use target array in build.rs

--- build.rs
+++ build.rs
@@ -1,14 +1,15 @@
-#[rustfmt::skip]
+const JIT_TARGETS: [&str; 4] = [
+    "aarch64-unknown-linux-gnu",
+    "x86_64-unknown-linux-gnu",
+    "aarch64-apple-darwin",
+    "x86_64-apple-darwin",
+];
+
 fn main() {
-  let target = std::env::var("TARGET").unwrap();
+    let target = std::env::var("TARGET").unwrap();
+    let jit_supported = JIT_TARGETS.contains(&target.as_str());
 
-  let jit_supported =
-      target == "aarch64-unknown-linux-gnu" ||
-      target == "x86_64-unknown-linux-gnu" ||
-      target == "aarch64-apple-darwin" ||
-      target == "x86_64-apple-darwin";
-
-  if jit_supported {
-    println!("cargo:rustc-cfg=feature=\"jit\"");
-  }
+    if jit_supported {
+        println!("cargo:rustc-cfg=feature=\"jit\"");
+    }
 }
