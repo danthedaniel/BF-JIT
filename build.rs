a1d9c2f6846c0eb2349beb36cfaba0031f33b9fe
Daniel Angell <me@danangell.com>
2025-06-03 07:23:57 +0100

    Add support for musl+linux

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
-const JIT_TARGETS: [&str; 4] = [
+const JIT_TARGETS: [&str; 6] = [
     "aarch64-unknown-linux-gnu",
+    "aarch64-unknown-linux-musl",
     "x86_64-unknown-linux-gnu",
+    "x86_64-unknown-linux-musl",
     "aarch64-apple-darwin",
     "x86_64-apple-darwin",
 ];
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
+    let target = std::env::var("TARGET").expect("TARGET env var not present");
     let jit_supported = JIT_TARGETS.contains(&target.as_str());
 
     if jit_supported {
